1b0b0f8489a07e42f47f008ef615b9b7bc0e8719
mohammedabdulQawi <111250313+mohammedabdulQawi@users.noreply.github.com>
2022-08-21 23:49:22 +0300

    Add files via upload

---
+++ Mohammed Abdul Qawi gahtan Homework2/src/Q8.java
@@ -0,0 +1,15 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class Q8 {
+    public static void main(String[] args) {
+        int x = 4;
+        //The Square
+        System.out.println("The Square is=\n" + (x * x));
+        //The Cube
+        System.out.println("The Cube is=\n" + (x * x * x));
+        //The Pow
+        System.out.println("The Fourth Power is=\n"+(Math.pow(x,4)));
+
+
+    }
+}
